4483122803965d9c88b2d99ac42b03dc9156f307
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-24 14:27:53 +0300

    made changes

---
+++ app/src/main/java/com/queen/finalspace/ui/CameraActivity.java
@@ -0,0 +1,65 @@
+package com.queen.finalspace.ui;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.queen.finalspace.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class CameraActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button btOpen;
+    String currentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        imageView = findViewById(R.id.captureImage);
+        btOpen = findViewById(R.id.btopen);
+
+        if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA) !=
+        PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            },
+                    100);
+        }
+
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 100){
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(captureImage);
+        }
+    }
+
+   private createFile
+}
