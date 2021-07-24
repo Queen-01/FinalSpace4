9152347ea930c85183036030bada77c9ddd9b377
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-24 12:56:41 +0300

    made changes

---
+++ app/src/main/java/com/queen/finalspace/ui/Camera.java
@@ -0,0 +1,44 @@
+package com.queen.finalspace.ui;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Camera extends AppCompatActivity {
+    ImageView imageView;
+    Button btOpen;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        imageView = findViewById(R.id.captureImage);
+        btOpen = findViewById(R.id.btopen);
+
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        if ( requestCode == REQUEST_IMAGE_CAPTURE && requestCode == RSESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
